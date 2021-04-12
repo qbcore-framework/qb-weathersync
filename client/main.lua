cc4c70ee2c1b6232279691d28a6be3d00175670b
LoveFistRP <qbcore@programmer.net>
2021-04-12 14:57:26 -0700

    thread update

--- client/main.lua
+++ client/main.lua
@@ -11,12 +11,9 @@ local disable = false
 QBCore = nil
 
 Citizen.CreateThread(function() 
-    while true do
-        Citizen.Wait(10)
-        if QBCore == nil then
-            TriggerEvent("QBCore:GetObject", function(obj) QBCore = obj end)    
-            Citizen.Wait(200)
-        end
+    while QBCore == nil do
+        TriggerEvent("QBCore:GetObject", function(obj) QBCore = obj end)    
+        Citizen.Wait(200)
     end
 end)
 
