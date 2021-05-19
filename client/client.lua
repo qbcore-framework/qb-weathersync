033b258b857709e40b41949a2d0743614125ea58
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-05-19 12:53:06 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -18,6 +18,7 @@ end)
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
 AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
     TriggerServerEvent('qb-weathersync:server:RequestStateSync')
+    TriggerServerEvent('qb-weathersync:server:RequestCommands')
 end)
 
 RegisterNetEvent('qb-weathersync:client:EnableSync')
@@ -29,6 +30,26 @@ RegisterNetEvent('qb-weathersync:client:SyncWeather')
 AddEventHandler('qb-weathersync:client:SyncWeather', function(NewWeather, newblackout)
     CurrentWeather = NewWeather
     blackout = newblackout
+end)
+
+RegisterNetEvent('qb-weathersync:client:RequestCommands')
+AddEventHandler('qb-weathersync:client:RequestCommands', function(isAllowed)
+    if isAllowed then
+        TriggerEvent('chat:addSuggestion', '/freezetime', _U('help_freezecommand'), {})
+        TriggerEvent('chat:addSuggestion', '/freezeweather', _U('help_freezeweathercommand'), {})
+        TriggerEvent('chat:addSuggestion', '/weather', _U('help_weathercommand'), {
+            { name=_U('help_weathertype'), help=_U('help_availableweather') }
+        })
+        TriggerEvent('chat:addSuggestion', '/blackout', _U('help_blackoutcommand'), {})
+        TriggerEvent('chat:addSuggestion', '/morning', _U('help_morningcommand'), {})
+        TriggerEvent('chat:addSuggestion', '/noon', _U('help_nooncommand'), {})
+        TriggerEvent('chat:addSuggestion', '/evening', _U('help_eveningcommand'), {})
+        TriggerEvent('chat:addSuggestion', '/night', _U('help_nightcommand'), {})
+        TriggerEvent('chat:addSuggestion', '/time', _U('help_timecommand'), {
+            { name=_U('help_timehname'), help=_U('help_timeh') }, 
+            { name=_U('help_timemname'), help=_U('help_timem') }
+        })
+    end
 end)
 
 Citizen.CreateThread(function()
