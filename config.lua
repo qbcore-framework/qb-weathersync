00a48b2aba7fd458d31c31feecdba0bec7d84500
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-26 21:56:30 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -11,8 +11,6 @@ Config.BlackoutVehicle  = false -- Set blackout affects vehicles                
 Config.NewWeatherTimer  = 10 -- Time (in minutes) between each weather change   default: 10
 Config.Disabled         = false -- Set weather disabled                         default: false
 
-Config.Locale           = 'en' -- Languages : en, fr, pt, tr, pt_br
-
 Config.AvailableWeatherTypes = { -- DON'T TOUCH EXCEPT IF YOU KNOW WHAT YOU ARE DOING
     'EXTRASUNNY',
     'CLEAR',
