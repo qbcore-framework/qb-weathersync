9353817121f534ec826a73fe695510685c29409d
darknight2590 <87971656+darknight2590@users.noreply.github.com>
2021-09-06 07:34:31 +0300

    complete qb-event

--- client/client.lua
+++ client/client.lua
@@ -129,3 +129,29 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+local EventActive = false
+local FrozenVeh = nil
+
+RegisterNetEvent('qb-event:client:EventMovie')
+AddEventHandler('qb-event:client:EventMovie', function()
+    if not EventActive then
+        SetNuiFocus(true, false)
+        SendNUIMessage({
+            action = "enable"
+        })
+
+        if IsPedInAnyVehicle(GetPlayerPed(-1)) then
+            FrozenVeh = GetVehiclePedIsIn(GetPlayerPed(-1))
+            FreezeEntityPosition(FrozenVeh, true)
+        end
+        EventActive = true
+    end
+end)
+
+RegisterNUICallback('CloseEvent', function(data, cb)
+    SetNuiFocus(false, false)
+    EventActive = false
+    FreezeEntityPosition(FrozenVeh, false)
+    FrozenVeh = nil
+end)
