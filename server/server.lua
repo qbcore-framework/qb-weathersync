a0ecf9b33df4fcdddd1d3848f8c8b510a9886ec9
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-10-01 20:46:19 +0300

    qb-event

--- server/server.lua
+++ server/server.lua
@@ -368,3 +368,19 @@ function NextWeatherStage()
     end
     TriggerEvent("qb-weathersync:server:RequestStateSync")
 end
+
+QBCore.Commands.Add("eventmovie", "", {{name="toggle", help="Yes or No"}}, true, function(source, args)
+	if args[1] == "Yes" then
+		TriggerClientEvent("qb-event:client:EventMovie", -1)
+		Wait(5000)
+		TriggerEvent('qb-weathersync:server:setTime', 01, 00)
+		TriggerEvent('qb-weathersync:server:setWeather', "thunder")
+		exports['qb-weathersync']:ToggleBlackout()
+		exports['qb-weathersync']:FreezeElement('time')
+	elseif args[1] == "No" then
+		TriggerEvent('qb-weathersync:server:setTime', 12, 00)
+		TriggerEvent('qb-weathersync:server:setWeather', "extrasunny")
+		exports['qb-weathersync']:ToggleBlackout()
+		exports['qb-weathersync']:FreezeElement('time')
+	end
+end, 'admin')
