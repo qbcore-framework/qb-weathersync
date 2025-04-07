0f5ef1b74ce89588479249cdddc41f8c3fd59248
Dispersia Roleplay <155019886+DispersiaRoleplay@users.noreply.github.com>
2025-04-07 22:41:13 +0300

    Real-Time sync fix + ua.lua translation
    Enhanced weather and time sync for QBCore.
    
     Replaced deprecated worldtimeapi.org with reliable TimeZoneDB API  
     Real-time accuracy synced every 1 minute  
     Fallback to OS time in case of API failure  
     Added Ukrainian localization file: ua.lua

---
+++ locales/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Kakarot'
+description 'Syncs the time & weather for all players on the server and allows editing by command'
+version '2.1.1'
+
+shared_scripts {
+    'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua'
+}
+
+server_script 'server/server.lua'
+client_script 'client/client.lua'
